af6b6d5b1c90fb628baeae1a4afbc8578c9c4a83
Chloe Steffens <caforonda103@yahoo.com>
2020-10-08 22:26:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function fetchBooks() {
 
 fetch('https://anapioficeandfire.com/api/books')
   .then(resp => resp.json())
-  .then(json => renderBooks(books));
+  .then(json => renderBooks(json));
 }
 
 function renderBooks(books) {
